419c8666f08ba3c8df199d253916060d24a0dc8a
William Parry <williamparry@gmail.com>
2023-12-26 10:24:08 +0000

    formatting

--- packages/web/src/Grid.tsx
+++ packages/web/src/Grid.tsx
@@ -1,39 +1,52 @@
-import { useEffect, useState } from 'react';
-import { ReactGrid, Highlight, Column, Row, CellChange } from "@silevis/reactgrid";
-import { createClient } from '@supabase/supabase-js';
-import { ToastContainer, toast } from 'react-toastify';
+import { useEffect, useState } from 'react'
 import {
-    useParams
-} from "react-router-dom";
-import "@silevis/reactgrid/styles.css";
-import 'react-toastify/dist/ReactToastify.css';
+    ReactGrid,
+    Highlight,
+    Column,
+    Row,
+    CellChange,
+} from '@silevis/reactgrid'
+import { createClient } from '@supabase/supabase-js'
+import { ToastContainer, toast } from 'react-toastify'
+import { useParams } from 'react-router-dom'
+import '@silevis/reactgrid/styles.css'
+import 'react-toastify/dist/ReactToastify.css'
 
-const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_ANON_KEY
+)
 
 function Grid() {
-    const [rows, setRows] = useState<Row[]>([]);
-    const [columns, setColumns] = useState<Column[]>([]);
-    const [loading, setLoading] = useState(false);
-    const { guildId, gridSlug } = useParams();
+    const [rows, setRows] = useState<Row[]>([])
+    const [columns, setColumns] = useState<Column[]>([])
+    const [loading, setLoading] = useState(false)
+    const { guildId, gridSlug } = useParams()
 
     const notifyError = (error: any) => {
-        toast.error(`Error: ${error.message}`);
-    };
+        toast.error(`Error: ${error.message}`)
+    }
 
     async function loadGridData() {
-        
         const { data, error } = await supabase
             .from('gg_cells')
             .select('*')
-            .eq('guild_id', guildId); // Add And
+            .eq('guild_id', guildId) // Add And
 
-        let rows: Row[] = [];
-        let columns: Column[] = [];
+        let rows: Row[] = []
+        let columns: Column[] = []
 
         for (var i = 0; i < 100; i++) {
             rows.push({
                 rowId: `R-${i}`,
-                cells: Array(100).fill({}).map((c, x) => ({ type: "text", text: data?.find(d => d.gg_row == i && d.gg_column == x)?.gg_value || "" }))
+                cells: Array(100)
+                    .fill({})
+                    .map((c, x) => ({
+                        type: 'text',
+                        text:
+                            data?.find((d) => d.gg_row == i && d.gg_column == x)
+                                ?.gg_value || '',
+                    })),
             })
             columns.push({ columnId: `C-${i}` })
         }
@@ -43,73 +56,75 @@ function Grid() {
 
     async function handleCellsChanged(changes: CellChange[]) {
         // Prepare the new rows for optimistic update
-        let newRows = [...rows];
-    
+        let newRows = [...rows]
+
         // Prepare the data for batch server update
-        let updates = [];
-    
+        let updates = []
+
         for (const change of changes) {
             if (change.type === 'text') {
-                const { rowId, columnId, newCell } = change;
-                const rowIndex = parseInt((rowId as string).split('-')[1]);
-                const columnIndex = parseInt((columnId as string).split('-')[1]);
-    
+                const { rowId, columnId, newCell } = change
+                const rowIndex = parseInt((rowId as string).split('-')[1])
+                const columnIndex = parseInt((columnId as string).split('-')[1])
+
                 // Update the local state optimistically
-                newRows[rowIndex].cells[columnIndex] = { ...newRows[rowIndex].cells[columnIndex], type: 'text', text: newCell.text, validator: (text: string) => true };
-    
+                newRows[rowIndex].cells[columnIndex] = {
+                    ...newRows[rowIndex].cells[columnIndex],
+                    type: 'text',
+                    text: newCell.text,
+                    validator: (text: string) => true,
+                }
+
                 // Prepare the update for server
                 updates.push({
                     grid_slug: gridSlug,
                     guild_id: guildId,
                     gg_row: rowIndex.toString(),
                     gg_column: columnIndex.toString(),
-                    gg_value: newCell.text
-                });
+                    gg_value: newCell.text,
+                })
             }
         }
-    
+
         // Update the local state
-        setRows(newRows);
-    
+        setRows(newRows)
+
         // Batch update to the server
-        const { error } = await supabase.from('gg_cells').upsert(updates);
-    
+        const { error } = await supabase.from('gg_cells').upsert(updates)
+
         if (error) {
             // Handle error: Notify user and potentially revert changes
-            notifyError(error);
-            setRows([...rows]); // Reverting to original rows
+            notifyError(error)
+            setRows([...rows]) // Reverting to original rows
         }
     }
-    
 
     useEffect(() => {
-
-        setLoading(true);
-        loadGridData().finally(() => setLoading(false));
-        subscribeToChanges();
-
-    }, []);
-
-    
+        setLoading(true)
+        loadGridData().finally(() => setLoading(false))
+        subscribeToChanges()
+    }, [])
 
     function subscribeToChanges() {
         const cellsSubscription = supabase
             .channel('supabase_realtime')
-            .on('postgres_changes', { event: '*', schema: 'public', table: 'gg_cells' }, payload => {
-                loadGridData();
-            })
-            .subscribe();
-
-        
+            .on(
+                'postgres_changes',
+                { event: '*', schema: 'public', table: 'gg_cells' },
+                (payload) => {
+                    loadGridData()
+                }
+            )
+            .subscribe()
 
         // Clean up and unsubscribe when the component unmounts
         return () => {
-            cellsSubscription.unsubscribe();
-        };
+            cellsSubscription.unsubscribe()
+        }
     }
 
     if (loading) {
-        return <div>Loading...</div>;
+        return <div>Loading...</div>
     }
 
     return (
@@ -123,7 +138,7 @@ function Grid() {
             />
             <ToastContainer />
         </>
-    );
+    )
 }
 
-export default Grid;
+export default Grid
--- packages/web/src/Home.tsx
+++ packages/web/src/Home.tsx
@@ -1,66 +1,84 @@
-import { useEffect, useState } from 'react';
-import { ReactGrid, Highlight, Column, Row, CellChange } from "@silevis/reactgrid";
-import { createClient } from '@supabase/supabase-js';
-import { ToastContainer, toast } from 'react-toastify';
+import { useEffect, useState } from 'react'
 import {
-  useParams
-} from "react-router-dom";
-import "@silevis/reactgrid/styles.css";
-import 'react-toastify/dist/ReactToastify.css';
-import { Link, List, ListItem, ListItemButton, ListItemText, Paper, Typography } from '@mui/material';
+    ReactGrid,
+    Highlight,
+    Column,
+    Row,
+    CellChange,
+} from '@silevis/reactgrid'
+import { createClient } from '@supabase/supabase-js'
+import { ToastContainer, toast } from 'react-toastify'
+import { useParams } from 'react-router-dom'
+import '@silevis/reactgrid/styles.css'
+import 'react-toastify/dist/ReactToastify.css'
+import {
+    Link,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemText,
+    Paper,
+    Typography,
+} from '@mui/material'
 
-const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_ANON_KEY
+)
 
 function Home() {
-  const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false)
 
-  const [grids, setGrids] = useState<{
-    guild_id: string,
-    gridSlug: string,
-    grid_name: string
-  }[]>([]);
+    const [grids, setGrids] = useState<
+        {
+            guild_id: string
+            gridSlug: string
+            grid_name: string
+        }[]
+    >([])
 
-  useEffect(() => {
-    const fetchGrids = async () => {
+    useEffect(() => {
+        const fetchGrids = async () => {
+            const {
+                data: { user },
+            } = await supabase.auth.getUser()
 
-      const { data: { user } } = await supabase.auth.getUser()
+            if (user) {
+                const { data, error } = await supabase
+                    .from('gg_grids')
+                    .select('*')
+                //   .eq('created_by_id', user.id); // Adjust based on your data model
 
-      if (user) {
-        const { data, error } = await supabase
-          .from('gg_grids')
-          .select('*')
-        //   .eq('created_by_id', user.id); // Adjust based on your data model
-
-        if (error) {
-          console.error('Error fetching grids:', error);
-        } else {
-          console.log(data)
-          setGrids(data);
+                if (error) {
+                    console.error('Error fetching grids:', error)
+                } else {
+                    console.log(data)
+                    setGrids(data)
+                }
+            }
         }
-      }
-    };
 
-    fetchGrids();
-  }, []);
+        fetchGrids()
+    }, [])
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+    if (loading) {
+        return <div>Loading...</div>
+    }
 
-  return (
-    <Paper sx={{ maxWidth: 600, margin: 'auto', padding: 2 }}>
-      <Typography variant="h4" sx={{ marginBottom: 2 }}>
-        My Grids
-      </Typography>
-      <List>
-        {grids.map((grid, index) => (
-          <ListItemButton key={index} component="a">
-            <ListItemText primary={grid.grid_name} />
-          </ListItemButton>
-        ))}
-      </List>
-    </Paper>
-  );
+    return (
+        <Paper sx={{ maxWidth: 600, margin: 'auto', padding: 2 }}>
+            <Typography variant="h4" sx={{ marginBottom: 2 }}>
+                My Grids
+            </Typography>
+            <List>
+                {grids.map((grid, index) => (
+                    <ListItemButton key={index} component="a">
+                        <ListItemText primary={grid.grid_name} />
+                    </ListItemButton>
+                ))}
+            </List>
+        </Paper>
+    )
 }
 
-export default Home;
+export default Home
